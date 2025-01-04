ddb4d15f200c614800efc9bb8b1a08362703c126
Hyosik Philip Joo <lux1937@gmail.com>
2025-01-04 13:58:25 -0800

    Clean up modules

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -1,8 +1,8 @@
 const message = {
-    NULL_VALUE: '필요한 값이 없습니다.',
-    NOT_FOUND: '존재하지 않는 자원',
-    BAD_REQUEST: '잘못된 요청',
-    INTERNAL_SERVER_ERROR: '서버 내부 오류'
-}
+  NULL_VALUE: 'Null Value',
+  NOT_FOUND: 'Resource Not Found',
+  BAD_REQUEST: 'Bad Request',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+};
 
 export default message;
