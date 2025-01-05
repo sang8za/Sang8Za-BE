c9e33b3651884d76a2d7fab278b1fb4180648237
Hyosik Philip Joo <lux1937@gmail.com>
2025-01-05 11:32:09 -0800

    feat: property list router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 //router index file
 import { Router } from 'express';
 import UserRouter from './UserRouter';
+import PropertyRouter from './PropertyRouter';
 
 const router = Router();
 router.use('/user', UserRouter);
+router.use('/property', PropertyRouter);
 
 export default router;
